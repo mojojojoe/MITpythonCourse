cec323be49fb3bc2b8b76eacc9c015a9e724e9e9
Bruce R Graham <bruce_00000@live.co.uk>
2023-06-15 19:17:52 +0200

    WIP: serious attempt at compare merge, still not there yet

--- merge_sort3.py
+++ merge_sort3.py
@@ -1,17 +1,39 @@
+import logging
+
+def sort_merge(L, R):
+"""Here a list of length 1 or 2 is input"""
+# order the lists
+    if L[0] > L[1]:
+        L[1],L[0] = L[0],L[1]
+    if R[0] > R[1]:
+        R[0],R[1] = R[1],R[0]
+# merge by popping the smallest item between the top of the two lists
+    M = []
+    if L[0] < R[0]:
 
 
-
-def recursplit_compare_merge(list):
+def recurSplit_compare_merge(list):
     l = len(list)
-    if l % 2 == 1:
-        print("odd")
+    m = int(l / 2)
+    L = []  # odd when list is odd
+    R = []  # always even
+    L, R = list[:m], list[m:]
+    if l > 2:
+        recurSplit_compare_merge(L)
+        recurSplit_compare_merge(R)
     else:
-        m = l/2
-        L, R = list[:m], list[m:]
-    return list
+        while (L and R):
+            if L[0] > L[1]:
+                L[1], L[0] = L[0], L[1]
+            if R[0] > R[1]:
+                R[0], R[1] = R[1], R[0]
+            if L[0] < R[0]:
+                M.append(L.pop())
+            else:
+                M.append(R.pop())
 
 def sort(list):
-    return recursplit_compare_merge(list)
+    return recurSplit_compare_merge(list)
 
 def ask_for_list():
     n = int(input("How long is the list?\n"))
@@ -23,19 +45,19 @@ def ask_for_list():
         n -= 1
     return D
 
-def sort_or_break(args):
+def sort_or_None(args):
     if isinstance(args, list):
         return sort(args)
     else:
         print("Error:args not a list")
-        return 0
+        return None
 
 def main(args = None):
     if not args:
         ask = ask_for_list()
         main(ask)
     else:
-        sorted = sort_or_break(args)
+        sorted = sort_or_None(args)
         if sorted:
             for idx,item in enumerate(sorted):
                 print(f"{idx,item}\n")
